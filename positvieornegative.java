5d72071fb7cd599a6d7c51505b0cc9efa2c42b55
arun-123456 <48314530+arun-123456@users.noreply.github.com>
2019-03-07 21:45:36 -0800

    Create positvieornegative.java

---
+++ positvieornegative.java
@@ -0,0 +1,31 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+         int a;   
+         Scanner s= new Scanner(System.in);
+         a=s.nextInt();
+         
+        if (a < 0)
+        {
+            System.out.println("Negative");
+        }
+        else if ( a== 0)
+         {   System.out.println("Zero");
+
+         }
+        else
+          {  System.out.println("Positive");
+           
+          } 
+    
+}
+	
+}
